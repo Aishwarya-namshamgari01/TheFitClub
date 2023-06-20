a4583b851ffba4af5f5660dafc7926a9ea02e54e
Aishwarya <aishwarya.namshamgari@gmail.com>
2023-06-20 12:19:34 +0530

    second code push

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 import Github from '../../assets/github.png';
 import Instagram from '../../assets/instagram.png'
-import LinkedIn from '../../assets/linkedin.png'
+import LinkedIn from '../../assets/linkedin.png';
 import Logo from '../../assets/logo.png';
 
 
